b0f6e8e2bec015f964e51894597d08210c4be13a
radioDevCreation <radekrozmus@gmail.com>
2021-10-04 21:57:20 +0200

    newsletter to netlify

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -12,15 +12,21 @@ const NewsLetter = () => {
           <h4>
             I write to my friends every few weeks
           </h4>
-          <form
-          className="contact-form" 
-          name="contact"
-          method="post"
-          netlify-honeypot="bot-field"
-          data-netlify="true"
-          action="/success">
+          <form className="contact-form" name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true" action="/success">
             <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="contact" />
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control "
+            />
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Your email"
+            />
             <button type="submit" className="btn form-control submit-btn">
               subscribe
             </button>
